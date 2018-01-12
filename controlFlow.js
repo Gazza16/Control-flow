35ffde60925b0dd91adfb9b4e18de7c19671d4a3
Gaston Mollica <mollica.gaston@gmail.com>
2018-01-12 23:50:56 +1000

    control flow refined

--- controlFlow.js
+++ controlFlow.js
@@ -6,3 +6,21 @@ if (roomMate === 'Hayley') {
 
 else {console.log('You may not enter!')}
 
+	
+
+let carMake = 'Ferrari'
+let hasKeys = false
+
+if (carMake === 'Ferrari' && hasKeys) {
+	console.log('Let\'s ride')
+}
+
+else if (carMake === 'Ferrari' || hasKeys) {
+	console.log('You have a Ferrari but can\'t drive it')
+}
+
+else if (carMake === 'other' || !hasKeys) {
+	console.log('You have random keys. You can\'t drive the car')
+}
+
+else (console.log('Don\'t know'))
